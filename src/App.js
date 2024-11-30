b83657d247fc79d49b459e5d61efc31f2e1e965d
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2024-12-01 01:02:58 +0530

    Removing comment code

--- src/App.js
+++ src/App.js
@@ -136,30 +136,6 @@ function Box({ children }) {
 }
 
 /* Stateful Component */
-/* function WatchedBox() {
-  const [watched, setWatched] = useState(tempWatchedData);
-
-  const [isOpen2, setIsOpen2] = useState(true);
-
-  return (
-    <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen2((open) => !open)}
-      >
-        {isOpen2 ? "–" : "+"}
-      </button>
-      {isOpen2 && (
-        <>
-          <WatchedMoviesSummary watched={watched} />
-          <WatchedMovieList watched={watched} />
-        </>
-      )}
-    </div>
-  );
-} */
-
-/* Stateful Component */
 function MovieList({ movies }) {
   return (
     <ul className="list">
