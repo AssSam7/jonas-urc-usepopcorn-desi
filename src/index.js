13dfd01b1321c75f6848f3f53c05146770dda6fd
Aslam Mohammed <mohammadaslamparvez7@gmail.com>
2024-12-01 12:56:04 +0530

    Making the props as Component API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
 // import "./index.css";
 // import App from "./App";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="red" size={36} onSetRating={setMovieRating} />
+      <p>This movie was rating {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={10} />
+    <StarRating messages={["Terrible", "Bad", "Okay", "Good", "Excellent"]} />
+    <StarRating size={24} color="blue" className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
